6261bb1a5f54ff2dfadb02b42beeaed9448e75ba
root <root@ip-10-55-10-249.ec2.internal>
2025-03-30 08:57:26 +0000

    updated code

--- checkEvenNumbers.sh
+++ checkEvenNumbers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This script is used to print only even numbers
 for I in {1..10}; do
-    if $(expr $I % 2 -eq 0); then
+	if [ $(expr $I % 2) -eq 0 ]; then
         echo $I is Even Number
     else
         echo $I is ODD number
